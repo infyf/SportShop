de54375e3e47729aba588242ecdaf8730d516b5e
Vlad <150368423+Vladosjan@users.noreply.github.com>
2025-01-21 16:56:44 +0200

    Create regPerson.js

---
+++ DB/regPerson.js
@@ -0,0 +1,21 @@
+const {DataTypes} = require(sequelize); 
+const sequelize = require('C://Users//user//Desktop//DB-Server-sport//RegistrationDB.sql')
+const Item = sequelize.define('Item', { 
+    ID:{ 
+        type: DataTypes.Number,
+        allowNull: false
+    }, 
+    Name: { 
+        type: DataTypes.STRING,
+        allowNull: false
+    }, 
+    Email:{ 
+        type: DataTypes.STRING,
+        allowNull: false
+    } ,
+    Password:{ 
+        type: DataTypes.STRING,
+        allowNull: false
+    }
+});
+module.exports = Item;
