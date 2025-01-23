6e5dd934a2547abfdcff239280ff6ad3609b3286
infyf <138444902+infyf@users.noreply.github.com>
2025-01-23 11:03:33 +0200

    Update  slider App.js

--- App.js
+++ App.js
@@ -1,13 +1,18 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/navbard/Navbar";
 import AuthForm from "./components/Auth/AuthForm";
+import Slider from "./components/home/Slider";
+import Categories from './components/home/Category';
+
 
 function App() {
   return (
     <Router>
       <Navbar />
+      <Slider />
       <Routes>
         <Route path="/auth" element={<AuthForm />} />
+        <Route path="/category" element={<Categories />} />
       </Routes>
     </Router>
   );
