6da7e7f61116ec3389c7c054ce9c80eda87a0aed
infyf <138444902+infyf@users.noreply.github.com>
2025-02-01 14:08:04 +0200

    Create AddProducts.jsx

---
+++ public/pages/AddProducts.jsx
@@ -0,0 +1,10 @@
+
+function AddProducts() {
+  return (
+    <main className="max-w-4xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
+      <h1 className="text-2xl font-bold mb-6">Розміщення товару</h1>
+    </main>
+  )
+}
+
+export default AddProducts
