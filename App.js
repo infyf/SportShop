2bf222bc2b983d91b54cd4d88e9bf282e33d37d5
infyf <138444902+infyf@users.noreply.github.com>
2025-02-23 12:21:52 +0200

    Update App.js

--- App.js
+++ App.js
@@ -3,17 +3,19 @@ import Navbar from "./components/navbard/Navbar"
 import Home from "./pages/Home"
 import Catalog from "./pages/Catalog"
 import AddProducts from "./pages/AddProducts"
+import ProductPage from "./pages/ProductPage"
 
 function App() {
   return (
     <Router>
-      <div className="min-h-screen">
+      <div className="min-h-screen bg-gray-50">
         <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/catalog" element={<Catalog />} />
           <Route path="/add-products" element={<AddProducts />} />
           <Route path="/category/:categoryId" element={<Catalog />} />
+          <Route path="/product/:productId" element={<ProductPage />} />
         </Routes>
       </div>
     </Router>
