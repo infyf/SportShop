1eeb7612153e529e239e50b7dac74afb13587a22
infyf <138444902+infyf@users.noreply.github.com>
2025-03-12 18:55:41 +0200

    Update App.js

--- App.js
+++ App.js
@@ -4,21 +4,26 @@ import Home from "./pages/Home"
 import Catalog from "./pages/Catalog"
 import AddProducts from "./pages/AddProducts"
 import ProductPage from "./pages/ProductPage"
+import WishlistPage from "./pages/WishlistPage"
+import { WishlistProvider } from "./components/context/WishlistContext"
 
 function App() {
   return (
-    <Router>
-      <div className="min-h-screen bg-gray-50">
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/catalog" element={<Catalog />} />
-          <Route path="/add-products" element={<AddProducts />} />
-          <Route path="/category/:categoryId" element={<Catalog />} />
-          <Route path="/product/:productId" element={<ProductPage />} />
-        </Routes>
-      </div>
-    </Router>
+    <WishlistProvider>
+      <Router>
+        <div className="min-h-screen bg-gray-50">
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/catalog" element={<Catalog />} />
+            <Route path="/add-products" element={<AddProducts />} />
+            <Route path="/category/:categoryId" element={<Catalog />} />
+            <Route path="/product/:productId" element={<ProductPage />} />
+            <Route path="/wishlist" element={<WishlistPage />} />
+          </Routes>
+        </div>
+      </Router>
+    </WishlistProvider>
   )
 }
 
