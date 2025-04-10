65ac9e75f605ac72c24de426b0113922e780cb3e
Vlad <150368423+Vladosjan@users.noreply.github.com>
2025-04-10 17:16:21 +0300

    Create journalUsersActions.js

---
+++ security/journalUsersActions.js
@@ -0,0 +1,5 @@
+await fetch("/api/users-log", {
+    method: "POST", 
+    headers: {"Content-Type": "application/json"}, 
+    body: JSON.stringify({action:" Users is login", timestamp: Date.now()})
+})
