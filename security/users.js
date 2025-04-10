641537db6f10481ca4a33e43076c88d163d59aaa
Vlad <150368423+Vladosjan@users.noreply.github.com>
2025-04-10 17:14:53 +0300

    Create users.js

---
+++ security/users.js
@@ -0,0 +1,7 @@
+// /api/user-stats
+router.get("/api/user-stats", async (req, res) => {
+    const users = await User.find(); // з MongoDB
+    const active = users.filter(user => user.isActive).length; // або статус онлайн
+    res.json({ total: users.length, active });
+  });
+  
