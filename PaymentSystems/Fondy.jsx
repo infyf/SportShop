f89a0311fb8562e95832fcae19d799b11251eed0
Vlad <150368423+Vladosjan@users.noreply.github.com>
2025-05-03 16:49:18 +0300

    Create Fondy.jsx

---
+++ PaymentSystems/Fondy.jsx
@@ -0,0 +1,32 @@
+const styles = {
+  form: {
+    width: '400px',
+    margin: 'auto',
+  },
+  cardElement: {
+    fontSize: '16px',
+    color: '#32325d',
+  },
+  submitButton: {
+    marginTop: '16px',
+    padding: '10px 15px',
+    backgroundColor: '#5cb85c',
+    color: '#fff',
+    border: 'none',
+    borderRadius: '4px',
+    cursor: 'pointer',
+    transition: 'background-color 0.3s',
+  },
+  disabledButton: {
+    backgroundColor: '#b3b3b3', // Adjust the color for disabled state
+    cursor: 'not-allowed',
+  },
+  error: {
+    color: 'red',
+    marginTop: '8px',
+  },
+  success: {
+    color: 'green',
+    marginTop: '8px',
+  },
+};
