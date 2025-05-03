0e28940c13e332147dbe8f60fecead08b66c64cd
Vlad <150368423+Vladosjan@users.noreply.github.com>
2025-05-03 16:50:21 +0300

    Update Fondy.jsx

--- PaymentSystems/Fondy.jsx
+++ PaymentSystems/Fondy.jsx
@@ -1,32 +1,29 @@
-const styles = {
-  form: {
-    width: '400px',
-    margin: 'auto',
-  },
-  cardElement: {
-    fontSize: '16px',
-    color: '#32325d',
-  },
-  submitButton: {
-    marginTop: '16px',
-    padding: '10px 15px',
-    backgroundColor: '#5cb85c',
-    color: '#fff',
-    border: 'none',
-    borderRadius: '4px',
-    cursor: 'pointer',
-    transition: 'background-color 0.3s',
-  },
-  disabledButton: {
-    backgroundColor: '#b3b3b3', // Adjust the color for disabled state
-    cursor: 'not-allowed',
-  },
-  error: {
-    color: 'red',
-    marginTop: '8px',
-  },
-  success: {
-    color: 'green',
-    marginTop: '8px',
-  },
-};
+import React, {useState} from "react"; 
+// Create a object of Fondy 
+const Fondy = () => 
+{ 
+    const [payUrl, setPayUrl] = useState(""); 
+    // create the handle function for this 
+    const handlePay = async () =>{ 
+        const resp = await fetch("https://api.fondy.eu/api/checkout/url/", { 
+            method:"POST",
+            headers:{"Content-Type": "application/json"}, 
+            body: JSON.stringify({ 
+                merchant_id: "1397120", 
+                order_id: `order_${Date.now()}`,
+                currency: "UAH", 
+                amount: 10000, 
+                resp_url: "http://localhost:3000/catalog",
+            }),
+        }); 
+        // create data variable 
+        const data =await resp.json();
+        setPayUrl(data.resp.checkout_url);
+    }; 
+    return( 
+        <div> 
+            <button onClick={handlePay}>Pay</button>
+            {payUrl && <a href={payUrl} target = "_blank">Go for pay</a>}
+        </div>
+    )
+}; export default Fondy;
